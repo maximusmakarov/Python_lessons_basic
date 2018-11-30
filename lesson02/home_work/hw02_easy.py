5e4662cb22bba0c56b8ce987baed0a5cdffc1f1c
Maxim Makarov <maximus.makarov@mail.ru>
2018-11-30 19:16:18 +0300

    решены все задачи легкого уровня

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -19,17 +19,27 @@ for fruit in fruits:
     print(n, '.' + '{:>12}'.format(fruit))
     n += 1
 
-
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
+from typing import List
 
-fruits = ['apple', 'banana', 'kiwi', 'watermelon', 'pineapple']
-berries = ['watermelon', 'melon', 'strawberry', 'blackberry', ]
-
-
+fruits = ['apple', 'bananas', 'kiwi', 'watermelon', 'melon']
+berrys = ['watermelon', 'raspberry', 'strawberry', 'blackberry', 'melon', 'blueberry', 'wild strawberry']
+result = list(set(fruits) - set(berrys))
+print(result)
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_numbers = []
+for digit in numbers:
+    if digit % 2 == 0:
+        digit = digit/4
+    else:
+        digit = digit * 2
+    new_numbers.append(digit)
+print(new_numbers)
